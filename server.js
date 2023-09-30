540c61ccd8b302ac5c0443a9d24ae39c29b0c30f
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-29 16:13:42 -0800

    updated server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -13,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
+
+const sequelize = require('./config/connection'); // Simply require your connection module
 
 const sess = {
   secret: 'Super secret secret',
@@ -37,5 +37,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
 });
